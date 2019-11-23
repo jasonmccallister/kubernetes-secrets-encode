734a3c6f1f24b63d974a103326bf35fe6f12ec45
Jason McCallister <themccallister@gmail.com>
2019-11-23 11:59:40 -0500

    init

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/base64"
+	"errors"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type secretsYAML struct {
+	APIVersion string            `yaml:"apiVersion"`
+	Kind       string            `yaml:"kind"`
+	Data       map[string]string `yaml:"data"`
+}
+
+func main() {
+	args := os.Args
+	if len(args) == 1 {
+		log.Fatal("missing")
+	}
+
+	fileArg := args[1]
+	file, err := ioutil.ReadFile(fileArg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	secrets := secretsYAML{}
+
+	yaml.Unmarshal(file, &secrets)
+
+	if secrets.Kind != "Secret" {
+		log.Fatal(errors.New("you must provide a secrets yaml"))
+	}
+
+	err = yaml.Unmarshal(file, &secrets)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	for k, v := range secrets.Data {
+		secrets.Data[k] = base64.StdEncoding.EncodeToString([]byte(v))
+	}
+
+	log.Println("showing encoded values")
+	for _, v := range secrets.Data {
+		log.Println(v)
+	}
+
+	// write to output file
+}
